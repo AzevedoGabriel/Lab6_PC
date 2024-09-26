d914edd3a0f9b4f9f8bcd3e61e7431a1e99ed893
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2024-09-26 16:22:12 -0300

    node

--- lab6/java/scenario1/Node.java
+++ lab6/java/scenario1/Node.java
@@ -1,8 +1,31 @@
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Node implements Runnable {
+
+    private final BlockingQueue<Task> taskQueue;
+    private final List<Task> executedTasks;
+
+    public Node(BlockingQueue taskQueue, List<Task> executedTasks) {
+      this.taskQueue = taskQueue;
+      this.executedTasks = executedTasks;
+    }
 
     @Override
     public void run() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'run'");  
+      while(true) {
+        try {
+          Task task = taskQueue.take();
+          System.out.println("Node processando tarefa: " + task.getId());
+
+          task.execute();
+          executedTasks.add(task);
+          System.out.println("Task " + task.getId() + " foi processada.");
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        
+
+      }
     }
 }
--- lab6/java/scenario1/ScenarioBase.java
+++ lab6/java/scenario1/ScenarioBase.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class ScenarioBase {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Scenario Base of Lab06!");
+        BlockingQueue<Task> taskQueue = new LinkedBlockingQueue<>();
+        List<TaskProducer> producers = new ArrayList<>();
+        List<Task> executedTasks = new CopyOnWriteArrayList<>();
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for(int i = 0; i < 5; i++)  {
+            TaskProducer producer = new TaskProducer(i, taskQueue, 5000)
+        }
     }
 }
