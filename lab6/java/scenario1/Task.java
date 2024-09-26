7a8845582e15b6446127ec80e020630e341d6bc7
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2024-09-26 16:32:04 -0300

    task

--- lab6/java/scenario1/Task.java
+++ lab6/java/scenario1/Task.java
@@ -2,13 +2,29 @@ import java.util.Random;
 
 public class Task {
     long id;
+    private final long creationTime;
+    private final int producerId;
 
-    public Task(long id) {
+    public Task(long id, int producerId) {
         this.id = id;
+        this.producerId = producerId;
+        this.creationTime = System.currentTimeMillis();
     }
 
     public long getId() {
         return this.id;
+    }
+
+    public long gettCreationTime() {
+        return this.creationTime;
+    }
+
+    public long getActiveTime() {
+        return System.currentTimeMillis() - creationTime;
+    }
+
+    public int getProducerId() {
+        return this.producerId;
     }
 
     public void execute() {
--- lab6/java/scenario1/TaskProducer.java
+++ lab6/java/scenario1/TaskProducer.java
@@ -1,8 +1,28 @@
+import java.util.concurrent.*;
+
 public class TaskProducer implements Runnable {
+    private final BlockingQueue<Task> taskQueue;
+    private final long productionInterval;
+    private final int id;
+    private long idCounter = 0;
+
+    public TaskProducer(int id, BlockingQueue taskQueue, long productionInterval) {
+        this.id = id;
+        this.taskQueue = taskQueue;
+        this.productionInterval = productionInterval;
+    }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        while (true) {
+            try {
+                Task task = new Task(++idCounter, id);
+                taskQueue.put(task);
+                System.out.println("Tarefa " + task.getId() + " produzida.");
+                Thread.sleep(productionInterval);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
